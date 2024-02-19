e4055f637e3c5171c2be8b62213d345b52117712
parkjh <ace2006jh@naver.com>
2024-02-19 12:41:31 +0900

    처음 만들어본 자바코드(사실아님)

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
+import java.sql.SQLOutput;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("캬!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("hi Java!");
+        System.out.println("HOla Intellij!");
+        System.out.println("배고픔");
+        System.out.println("심심함");
+        System.out.println("로아 해야함 ㄷㄷ");
+
     }
 }
---
+++ src/Main3.java
@@ -0,0 +1,11 @@
+public class Main3 {
+    //하하하하하
+    public static void main(String[] args) {
+        System.out.println("아");
+        System.out.println("뭐하지");
+        System.out.println("노트패드로 깔짝였던게 엊그제 같은데");
+        System.out.println("아아아아아");
+        System.out.println("할게 없엉");
+
+    }
+}
