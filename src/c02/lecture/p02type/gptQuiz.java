535fea9ea27762da3ce410fc3577b8702dc6e858
parkjh <ace2006jh@naver.com>
2024-02-19 17:28:02 +0900

    gpt quiz

---
+++ src/c02/lecture/p02type/gptQuiz.java
@@ -0,0 +1,14 @@
+package c02.lecture.p02type;
+
+public class gptQuiz {
+    public static void main(String[] args) {
+
+
+        int a = 12;
+        int b = 26;
+        int c = 132;
+        int average = (a + b + c) / 3;
+        System.out.println(average);
+
+    }
+}
